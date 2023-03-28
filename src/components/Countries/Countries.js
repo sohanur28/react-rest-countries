d42ce250c0bba2d303d28d4b272a1f1dc14dc799
sohanur28 <gsrahman28@gmail.com>
2023-03-28 22:38:57 +0600

    country

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Countries = () => {
+    const [countries, setCountries] = useState([]);
+
+    useEffect( ()=>{
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => setCountries(data))
+    }, [])
+
     return (
         <div>
-            <h1>Hello from Countries.</h1>
+            <h1>Hello from Countries: {countries.length}</h1>
+            {
+                countries.map(country => console.log(country))
+            }
         </div>
     );
 };
