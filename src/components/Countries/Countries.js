e5b7d057c5d76a53705cb2e491c88f8992ad98af
sohanur28 <gsrahman28@gmail.com>
2023-03-28 23:38:16 +0600

    uniqeKey

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Country from '../Country/Country';
+import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
@@ -13,14 +14,14 @@ const Countries = () => {
     return (
         <div>
             <h1>Hello from Countries: {countries.length}</h1>
-            {/* {
-                countries.map(country => console.log(country))
-            } */}
+            <div className='countries-container'>
             {
                 countries.map(country => <Country 
                     country={country}
+                    key={country.cca3}
                     ></Country>)
             }
+            </div>
         </div>
     );
 };
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './Country.css'
 
 const Country = (props) => {
-    const {area, region, name} = props.country;
+    const {area, region, name, flags} = props.country;
     return (
         <div className='country'>
             <h2>Name: {name.common}</h2>
+            <img src={flags.png} alt="" />
             <p>Area: {area}</p>
             <p><small>Region: {region}</small></p>
         </div>
