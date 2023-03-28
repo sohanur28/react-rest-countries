7e8503a8ef187867c16d9ac2fd02758698e83309
sohanur28 <gsrahman28@gmail.com>
2023-03-28 22:59:30 +0600

    childCompnt

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Country from '../Country/Country';
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
@@ -14,6 +15,13 @@ const Countries = () => {
             <h1>Hello from Countries: {countries.length}</h1>
             {
                 countries.map(country => console.log(country))
+            }
+            {
+                countries.map(country => <Country 
+                    name={country.name.common} 
+                    area={country.area}
+                    population={country.population}
+                    ></Country>)
             }
         </div>
     );
