b9f738ac668712e1b2202267da23fcfee803d1c3
sohanur28 <gsrahman28@gmail.com>
2023-03-28 17:46:13 +0600

    folderStrcture

--- src/App.js
+++ src/App.js
@@ -23,6 +23,18 @@ function LaodCountries(){
     <div>
       <h1>Visiting Every country of the world!!!</h1>
       <h3>Available Countries: {countries.length}</h3>
+      {
+        countries.map(country => <Country name={country.name.common} population={country.population}></Country>)
+      }
+    </div>
+  )
+}
+
+function Country(props){
+  return (
+    <div>
+      <h2>Name: {props.name}</h2>
+      <h4>Population: {props.population}</h4>
     </div>
   )
 }
